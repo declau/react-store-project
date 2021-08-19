6b087f55a8dfd77d95f0cf7001a2ea534a7e0990
declau <dec82@hotmail.com>
2021-08-19 18:34:46 -0300

    feat: init project

---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+    return (
+        <>
+           <h1 className="jumbotron text-center bg-primary">Hello :)</h1> 
+           <p>From Next js...</p>
+        </>
+    );
+}
+
+export default Index;
