1932dbfdc6d478620c7f6089e4357ce2a29eae91
declau <dec82@hotmail.com>
2021-08-20 14:32:41 -0300

    feat: Add register form

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,10 +1,55 @@
+import { useState } from "react";
+
 const Register = () => {
-    return (
-        <>
-            <h1 className="jumbotron text-center bg-primary square">Register</h1>
-            <p>register page</p>
-        </>
-    )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ name, email, password });
+  };
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">Register</h1>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="form-control mb-4 p-4"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter name"
+            required
+          />
+
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter Email"
+            required
+          />
+
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter Password"
+            required
+          />
+
+          <div className="d-grid gap-2">
+            <button type="submit" className="btn btn-primary btn-lg">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Register;
