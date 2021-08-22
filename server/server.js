90e373f27c118f87fcf7910c5a144cb67cd88b27
declau <dec82@hotmail.com>
2021-08-22 17:39:04 -0300

    feat: finish asic server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
-console.log("server setup");
+import express from "express";
+import cors from "cors";
+const morgan = require("morgan");
+require("dotenv").config();
+
+//Create express app
+const app = express();
+
+// Add middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log("Middleware working...");
+  next();
+});
+
+// route
+app.get("/", (req, res) => {
+  res.send("hit server endpoint");
+});
+
+//Port
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
