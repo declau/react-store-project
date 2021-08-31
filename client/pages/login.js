802cf6822ebc10f754f3593be84ed392d5e924a6
declau <dec82@hotmail.com>
2021-08-31 19:15:01 -0300

    feat: Login page

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,10 +1,73 @@
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
+
 const Login = () => {
-    return (
-        <>
-            <h1 className="jumbotron bg-primary text-center square">Login</h1>
-            <p>login page</p>
-        </>
-    )
-}
+  const [email, setEmail] = useState("dec@dec.com");
+  const [password, setPassword] = useState("dddddd");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.table({ name, email, password });
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/login`, {
+        email,
+        password,
+      });
+      console.log("LOGIN RESPONSE", data);
+      //   toast.success("Registration successful. Please login.");
+      //   setLoading(false);
+    } catch (err) {
+      toast.error(err.response.data);
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">Login</h1>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter Email"
+            required
+          />
+
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter Password"
+            required
+          />
+
+          <div className="d-grid gap-2">
+            <button
+              type="submit"
+              className="btn btn-primary btn-lg"
+              disabled={!email || !password || loading}
+            >
+              {loading ? <SyncOutlined spin /> : "Submit"}
+            </button>
+            <p className="text-center p-3">
+              Not yet registered?{" "}
+              <Link href="/register">
+                <a>Register</a>
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Login;
